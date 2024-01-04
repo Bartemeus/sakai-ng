03fde52946121033da598dbf45c9763bce381011
Nerzul <nerkur@bk.ru>
2024-01-05 00:20:43 +0600

    аукцион

--- src/app/demo/components/landing/landing.component.ts
+++ src/app/demo/components/landing/landing.component.ts
@@ -10,6 +10,15 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
 export class LandingComponent {
     messageToSend: string;
     messages: string[] = [];
+    lots: any[] = [{
+      title:'Asus ROG Strix',
+      img:'https://api.technodom.kz/f3/api/v1/images/257922_2.jpg',
+      price:600000,
+      param1:'AMD Ryzen 7 AM4 5800X',
+      param2:'NVIDIA GeForce RTX 3060 8 ГБ',
+      param3:'Материнская плата ASUS B550',
+      param4:'1 ТБ (SSD) DDR4 32 ГБ 4 x 8 ГБ'
+    }];
     constructor(public layoutService: LayoutService, public router: Router, private socket: Socket) {
       this.socket.on('message', (data: string) => {
         this.messages.push(data);
