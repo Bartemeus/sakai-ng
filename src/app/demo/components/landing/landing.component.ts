f1881854742057403c3dd63665355ee553d2bd36
Nerzul <nerkur@bk.ru>
2024-02-25 13:08:35 +0600

    landing

--- src/app/demo/components/landing/landing.component.ts
+++ src/app/demo/components/landing/landing.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Socket } from 'ngx-socket-io';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
     selector: 'app-landing',
     templateUrl: './landing.component.html',
-    styles:`.dropdown-content {
+    styles: `.dropdown-content {
       display: none !important;
       position: absolute; /* Позиционирование относительно родительского элемента */
       background-color: #f9f9f9; /* Цвет фона */
@@ -18,40 +17,60 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
   .dropdown-content.visible {
       display: block !important;
   }
-  `
+  `,
 })
 export class LandingComponent {
     messageToSend: string;
     messages: string[] = [];
-    lots: any[] = [{
-      title:'Asus ROG Strix',
-      img:'https://api.technodom.kz/f3/api/v1/images/257922_2.jpg',
-      price:600000,
-      param1:'AMD Ryzen 7 AM4 5800X',
-      param2:'NVIDIA GeForce RTX 3060 8 ГБ',
-      param3:'Материнская плата ASUS B550',
-      param4:'1 ТБ (SSD) DDR4 32 ГБ 4 x 8 ГБ'
-    }];
-    constructor(public layoutService: LayoutService, public router: Router, private socket: Socket) {
-      // this.socket.on('message', (data: string) => {
-      //   this.messages.push(data);
-      // });
-     }
-     isDropdownVisible = false;
-
-  toggleDropdown() {
-    this.isDropdownVisible = !this.isDropdownVisible;
-  }
-  ngOnInit() {
-    this.socket.on('msg', (data: string) => {
-        this.messages.push(data);
-    });
-  }
-
-  sendMessage(): void {
-    if (this.messageToSend.trim() !== '') {
-      this.socket.emit('message', this.messageToSend);
-      this.messageToSend = ''; // Очистить поле ввода после отправки
+    lots: any[] = [
+        {
+            title: 'Asus ROG Strix',
+            img: 'https://api.technodom.kz/f3/api/v1/images/257922_2.jpg',
+            price: 600000,
+            param1: 'AMD Ryzen 7 AM4 5800X',
+            param2: 'NVIDIA GeForce RTX 3060 8 ГБ',
+            param3: 'Материнская плата ASUS B550',
+            param4: '1 ТБ (SSD) DDR4 32 ГБ 4 x 8 ГБ',
+        },
+        {
+            title: 'Asus ROG Strix',
+            img: 'https://api.technodom.kz/f3/api/v1/images/257922_2.jpg',
+            price: 600000,
+            param1: 'AMD Ryzen 7 AM4 5800X',
+            param2: 'NVIDIA GeForce RTX 3060 8 ГБ',
+            param3: 'Материнская плата ASUS B550',
+            param4: '1 ТБ (SSD) DDR4 32 ГБ 4 x 8 ГБ',
+        },
+        {
+            title: 'balaqai',
+            img: 'https://frankfurt.apollo.olxcdn.com/v1/files/o9o45jpxdovx-KZ/image;s=900x900',
+            price: 5690,
+            param1: 'Детские подгузники',
+            param2: 'Жапондық технологиямен',
+            param3: 'Подгузники “BALAQAI”',
+            param4: '5 690 ₸',
+        },
+    ];
+    constructor(public layoutService: LayoutService, public router: Router) {
+        // this.socket.on('message', (data: string) => {
+        //   this.messages.push(data);
+        // });
     }
-  }
+    isDropdownVisible = false;
+
+    toggleDropdown() {
+        this.isDropdownVisible = !this.isDropdownVisible;
+    }
+    ngOnInit() {
+        // this.socket.on('msg', (data: string) => {
+        //     this.messages.push(data);
+        // });
+    }
+
+    sendMessage(): void {
+        // if (this.messageToSend.trim() !== '') {
+        //   this.socket.emit('message', this.messageToSend);
+        //   this.messageToSend = ''; // Очистить поле ввода после отправки
+        // }
+    }
 }
